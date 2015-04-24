3e4e3be3367f87e224ca50690c9ef31579de8a2c
Ama Sanchez <asanchez@harding.edu>
2015-04-23 21:49:33 -0500

    Camera follows player around.

--- Assets/Scripts/CamController.cs
+++ Assets/Scripts/CamController.cs
@@ -1,23 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CamController : MonoBehaviour {
-   public GameObject target;  // The player
-   private Vector3 offset;
+public class CamController : MonoBehaviour
+{
+   private Transform player;
 
-	// Use this for initialization
-	void Start () {
-      offset = new Vector3(0f, 0f, -10f);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-   void LateUpdate()
+   void Start()
    {
-      // Change camera's position to the same as the player (with the z-value of -10)
-      transform.position = target.transform.position + offset;
+      player = GameObject.Find("Player").transform;
+   }
+
+   void Update()
+   {
+      Vector3 playerpos = player.position;
+      playerpos.z = transform.position.z;
+      transform.position = playerpos;
    }
 }

35ba9b42f421ee426669b446f8861791e24bab1e
Ama Sanchez <asanchez@harding.edu>
2015-04-26 22:15:17 -0500

    Added Celtic music as background music.

---
+++ Assets/Scripts/GameMusic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMusic : MonoBehaviour {
+
+   private static GameMusic instance = null;
+
+   public AudioClip NewMusic;
+   public static GameMusic Instance
+   {
+      get { return instance; }
+   }
+   void Awake() {
+     if (instance != null && instance != this) {
+         Destroy(this.gameObject);
+         return;
+     } else {
+         instance = this;
+     }
+     DontDestroyOnLoad(this.gameObject);
+   }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+   static bool AudioBegin = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   void Awake () {
+      if (!AudioBegin)
+      {
+         GameObject go = GameObject.Find("GameMusic");
+         //Finds the game object called Game Music, if it goes by a different name, change this. 
+
+         go.GetComponent<AudioSource>().Play(); //Plays the audio.
+         DontDestroyOnLoad(gameObject);
+         AudioBegin = true;
+      }
+       
+   }
+}
